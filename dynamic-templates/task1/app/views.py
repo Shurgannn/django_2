2d26cfadc59fa0155fd427e08399d57fd88ec20d
Aleksandr <shurgan_nn@bk.ru>
2019-10-30 11:53:29 +0300

    первые правки

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,24 @@
+import csv
+from pprint import pprint
+
 from django.shortcuts import render
 
 def inflation_view(request):
     template_name = 'inflation.html'
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
+    data_list = []
+    dict_keys = ''
 
-    return render(request, template_name,
-                  context)
+    # чтение csv-файла и заполнение контекста
+    with open('inflation_russia.csv', encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            data_dict = {'Год': row['Год'], 'Янв': row['Янв'], 'Фев': row['Фев'], 'Мар': row['Мар'], 'Апр': row['Апр'], 'Май': row['Май'], 'Июн': row['Июн'], 'Июл': row['Июл'], 'Авг': row['Авг'], 'Сен': row['Сен'], 'Окт': row['Окт'], 'Ноя': row['Ноя'], 'Дек': row['Дек'], 'Всего': row['Суммарная']}
+            dict_keys = data_dict.keys()
+            data_list.append(data_dict)
+    # pprint(data_list)
+    pprint(dict_keys)
+    context = {'data': data_list,
+               'list': dict_keys}
+
+    return render(request, template_name, context)
