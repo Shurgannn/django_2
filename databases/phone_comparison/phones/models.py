01c48f56e27ffb05457901119fe1d8339061a875
Aleksander Maksakov <shurgan_nn@bk.ru>
2019-12-25 16:41:24 +0300

    first committ

--- databases/phone_comparison/phones/models.py
+++ databases/phone_comparison/phones/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Phone(models.Model):
+    # TODO: Добавьте требуемые поля
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    operating_system = models.CharField(max_length=50)
+
+# class Nokiaa(Phone):
+#     flashlight = models.CharField(max_length=50)
+#
+#
+# class Iphone(Phone):
+#     finger_scaner = models.CharField(max_length=50)
+
+
--- databases/phone_comparison/phones/views.py
+++ databases/phone_comparison/phones/views.py
@@ -1,9 +1,32 @@
+import csv
+
 from django.shortcuts import render
+from phones.models import Phone
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    data_list = []
+    with open('phones.csv', encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for row in reader:
+            data_dict = {'Название': row['name'], 'Цена': row['price'], 'Операционнаясистема': row['operating_system']}
+            d_keys = data_dict.keys()
+            print(d_keys)
+    # data = csv.reader(open('phones.csv'), delimiter=';')
+    # for row in data:
+    #     if row[0] != 'id':
+    #         phone = Phone()
+    #         phone.id = row[0]
+    #         phone.name = row[1]
+    #         phone.operating_system = row[3]
+    #         phone.price = row[2]
+    #         phone.save()
+    # serv_list = Phone.objects.all()
+            data_list.append(data_dict)
+    print(data_list)
+    context = {'data': data_list,
+               'list_k': d_keys}
     return render(
         request,
         template,
