e4a7247d056c833f95592363a380ef288cd67387
Aleksander Maksakov <shurgan_nn@bk.ru>
2020-02-08 03:14:08 +0300

    2 committ

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.views.generic import ListView
 from django.shortcuts import render
 
@@ -6,7 +8,8 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    context = {'object_list': Article.objects.all()}
+
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -6,10 +6,19 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
+    context = {'object_list': Student.objects.all()}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
 
     return render(request, template, context)
+
+# {% for student in object_list %}
+#     <li>{{ student.name }} {{ student.group }}
+#
+#       {% for obj_teacher in object.teachers.all %}
+#     <p>{{ obj_teacher.name }}  {{ obj_teacher.subject }}</p>
+#       {% endfor %}
+#       <br> Преподаватель: {{ student.teacher.name }} {{ student.teacher.subject }}</li>
+#   {% endfor %}
