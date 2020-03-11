981588edb688dc7c72793d37da52bb654c084ec5
Aleksander Maksakov <shurgan_nn@bk.ru>
2020-03-11 21:13:47 +0300

    5 committ

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Thematics(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    thematic = models.ManyToManyField(Thematics, through='Relationship', through_fields=('article', 'thematics'),)
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +26,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Relationship(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    thematics = models.ForeignKey(Thematics, on_delete=models.CASCADE)
+
